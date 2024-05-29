79051aecefdfbd98e8ec10d917d61fed5d3d33cc
Matheus Markies <matheus.markies@gmail.com>
2024-05-29 20:02:15 -0300

    Added: AlertResponse

--- src/main/java/com/brasens/response/AlertResponse.java
+++ src/main/java/com/brasens/response/AlertResponse.java
@@ -1,19 +1,23 @@
 package com.brasens.response;
 
+import com.brasens.dtos.enums.AlertLevel;
+
 public class AlertResponse {
     private int id;
     private String value;
     private String current;
     private String target;
+    private String level;
 
     public AlertResponse() {
     }
 
-    public AlertResponse(int id, String value, String current, String target) {
+    public AlertResponse(int id, String value, String current, String target, String level) {
         this.id = id;
         this.value = value;
         this.current = current;
         this.target = target;
+        this.level = level;
     }
 
     public int getId() {
@@ -46,5 +50,13 @@ public class AlertResponse {
 
     public void setTarget(String target) {
         this.target = target;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
     }
 }
