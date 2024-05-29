17009191b91c731626c412a96f7255c644b372ae
Matheus Markies <matheus.markies@gmail.com>
2024-05-29 00:13:36 -0300

    Added: RequestBudget

--- src/main/java/com/brasens/requestbody/ClientRegisterRequest.java
+++ src/main/java/com/brasens/requestbody/ClientRegisterRequest.java
@@ -25,5 +25,46 @@ public class ClientRegisterRequest {
 
 		return client;
 	}
-	
+
+	public ClientRegisterRequest() {
+	}
+
+	public ClientRegisterRequest(String name, String email, String organization, String role) {
+		this.name = name;
+		this.email = email;
+		this.organization = organization;
+		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getOrganization() {
+		return organization;
+	}
+
+	public void setOrganization(String organization) {
+		this.organization = organization;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
 }
