19820559f1eace17774b4ed25449a10feb35b165
Matheus Markies <matheus.markies@gmail.com>
2024-06-20 13:27:31 -0300

    Added: PSD

--- src/main/java/com/brasens/dtos/PSD.java
+++ src/main/java/com/brasens/dtos/PSD.java
@@ -7,8 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.time.ZonedDateTime;
@@ -19,12 +17,12 @@ import java.util.UUID;
 import static com.brasens.Commons.DEFAULT_TIMEZONE;
 
 @Entity
-@Table(name="FFT")
+@Table(name="PSD")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class FFT {
+public class PSD {
     @Id
     @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
     @GeneratedValue(generator = "UUIDGenerator")
@@ -37,32 +35,12 @@ public class FFT {
     @JsonIgnore
     private List<Vector> values = new ArrayList<>();
 
-    @Column(name = "asset_key", unique = true)
-    private String key;
-
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", timezone = "UTC")
     @Column(name = "added", insertable = false, updatable = false)
     private ZonedDateTime added = ZonedDateTime.now().withZoneSameInstant(DEFAULT_TIMEZONE.toZoneId());
 
-    //@OneToOne(mappedBy = "FFT", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    //@JsonIgnore
-    //private FFTStatisticalValues statisticalValues;
-
-    @OneToMany(targetEntity = FFTStatisticalValues.class, cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @JoinColumn(name = "statistical_values_id")
-    @JsonIgnore
-    private List<FFTStatisticalValues> statisticalValues = new ArrayList<>();
-
-    @OneToOne(mappedBy = "FFT", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @JsonIgnore
-    private PSD psd;
-
-    @OneToOne(mappedBy = "FFT", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
-    @JsonIgnore
-    private Envelope envelope;
-
     @OneToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "asset_id")
+    @JoinColumn(name = "fft_id")
     @JsonIgnore
-    private Asset asset;
+    private FFT fft;
 }
