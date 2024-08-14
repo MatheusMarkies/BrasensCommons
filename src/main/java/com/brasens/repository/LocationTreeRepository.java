72e19bc0bc4bc3f44b5e76602246969ebdc501eb
Matheus Markies <matheus.markies@gmail.com>
2024-08-14 15:21:31 -0300

    Added: Asset Info

---
+++ src/main/java/com/brasens/repository/LocationTreeRepository.java
@@ -0,0 +1,13 @@
+package com.brasens.repository;
+
+import com.brasens.dtos.History;
+import com.brasens.dtos.LocationTree;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface LocationTreeRepository extends JpaRepository<LocationTree, UUID> {
+    @Override
+    Optional<LocationTree> findById(UUID uuid);
+}
