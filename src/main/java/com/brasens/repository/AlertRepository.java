2064b3a60029c85f494fb3673c2ff67e922efca8
Matheus Markies <matheus.markies@gmail.com>
2024-08-21 10:34:15 -0300

    Added Asset Status, Alert Comments, Alert Status and Alerts Target

--- src/main/java/com/brasens/repository/AlertRepository.java
+++ src/main/java/com/brasens/repository/AlertRepository.java
@@ -9,5 +9,5 @@ import java.util.List;
 import java.util.UUID;
 @Repository
 public interface AlertRepository extends JpaRepository<Alert, UUID> {
-    List<Alert> findByKey(String key);
+
 }
--- src/main/java/com/brasens/repository/CriticalValuesRepository.java
+++ src/main/java/com/brasens/repository/CriticalValuesRepository.java
@@ -10,5 +10,5 @@ import java.util.UUID;
 
 @Repository
 public interface CriticalValuesRepository  extends JpaRepository<CriticalValues, UUID> {
-    List<CriticalValues> findByKey(String key);
+
 }
