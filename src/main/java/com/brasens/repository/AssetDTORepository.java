2d41f50f347a1da50757f0fb575ab26a8fa5766b
Matheus Markies <48259420+MatheusMarkies@users.noreply.github.com>
2025-08-27 22:46:16 -0300

    Added Asset Status, Alert Comments, Alert Status and Alerts Target

--- src/main/java/com/brasens/repository/AssetDTORepository.java
+++ src/main/java/com/brasens/repository/AssetDTORepository.java
@@ -1,6 +1,7 @@
 package com.brasens.repository;
 
 import com.brasens.dtos.Asset;
+import com.brasens.dtos.AssetSummaryDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,7 +9,7 @@ import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface AssetDTORepository extends JpaRepository<Asset, UUID> {
-    Optional<Asset> findByName(String name);
-    Optional<Asset> findByKey(String key);
+public interface AssetDTORepository extends JpaRepository<AssetSummaryDTO, UUID> {
+    Optional<AssetSummaryDTO> findByName(String name);
+    Optional<AssetSummaryDTO> findByKey(String key);
 }
