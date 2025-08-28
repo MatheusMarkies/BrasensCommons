18f141dcfaed133f472786647429fea9f25b87e2
Matheus Markies <48259420+MatheusMarkies@users.noreply.github.com>
2025-08-27 22:55:09 -0300

    Added Asset Status, Alert Comments, Alert Status and Alerts Target

--- src/main/java/com/brasens/dtos/AssetSummaryDTO.java
+++ src/main/java/com/brasens/dtos/AssetSummaryDTO.java
@@ -2,11 +2,16 @@ package com.brasens.dtos;
 
 import com.brasens.dtos.enums.AlertLevel;
 import com.brasens.dtos.enums.AssetState;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
@@ -14,23 +19,42 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name="AssetSummaryDTO")
 public class AssetSummaryDTO {
+    @Id
+    @GenericGenerator(name = "UUIDGenerator", strategy = "uuid2")
+    @GeneratedValue(generator = "UUIDGenerator")
+    @Column(name = "id")
+    @JsonIgnore
     private UUID id;
+    @Column(name = "name", unique = true)
     private String name;
+
+    @Column(name = "asset_key", unique = true)
     private String key;
+    @Column(name = "rpm", nullable = true)
     private double rpm;
+
+    @Column(name = "location")
     private String location;
-    private boolean isRigidBase;
-    private String type;
+    @Column(name = "base")
+    boolean isRigidBase;
+    @Column(name = "type")
+    private String type; //Pump, Motor, Fan
     private int power;
     private int pasNumber;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", timezone = "UTC")
+    @Column(name = "last_communication", insertable = false, updatable = false)
     private ZonedDateTime lastCommunication;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'", timezone = "UTC")
+    @Column(name = "added", insertable = false, updatable = false)
     private ZonedDateTime added;
+    @Column(name = "alert_level")
+    @Enumerated(EnumType.STRING)
+    @Type( type = "alertlevel")
     private AlertLevel level;
 
-    // These names must match the constructor parameters
     private String locationTreeName;
     private String organizationName;
-    private String bearingName;
-    private AssetState assetState;
 }
